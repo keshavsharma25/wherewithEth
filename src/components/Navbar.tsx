5e767c1781880c9ac233f35063cea0e243c6c626
Keshav Sharma <skeshav0825@gmail.com>
2022-11-28 20:03:52 +0530

    Create Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import { Flex, Heading, Img } from "@chakra-ui/react";
+import Link from "next/link";
+import { useAccount } from "wagmi";
+import { ConnectButtonCustom } from "./connectButtonCustom";
+
+export interface INavbarProps {}
+
+export function Navbar(props: INavbarProps) {
+  const { address, isConnected } = useAccount();
+
+  return (
+    <>
+      <Flex
+        alignItems="center"
+        justifyContent="space-between"
+        bgColor="#0C0C0D"
+        height="80px"
+      >
+        <Flex flexDirection={"row"} alignItems={"center"}>
+          <Link href="/">
+            <Img src="/eth.svg" alt="ethereum" ml={"20px"} />
+          </Link>
+          <Heading size="md" fontFamily="Arial" color={"whiteAlpha.900"}>
+            <Link href="/">wherewithETH</Link>
+          </Heading>
+        </Flex>
+        <Flex mr={8} alignItems="center">
+          <ConnectButtonCustom />
+        </Flex>
+      </Flex>
+    </>
+  );
+}
