1bd68d145dbdc0943564a4356dd6cbf764eec6a2
Keshav Sharma <skeshav0825@gmail.com>
2023-01-11 02:12:09 +0530

    Feat - created Moralis Helpers

---
+++ src/utils/moralisHelpers.ts
@@ -0,0 +1,19 @@
+import { moralisScanMapper } from "./helpers";
+
+export const getToken = async (address: string, chain: string) => {
+  const BASE_URL = "https://deep-index.moralis.io/api/v2/";
+
+  const url = `${BASE_URL}${address}/erc20?chain=${moralisScanMapper[chain]}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+      "X-API-Key": process.env.NEXT_PUBLIC_MORALIS_API_KEY!,
+    },
+  };
+
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  return data;
+};
