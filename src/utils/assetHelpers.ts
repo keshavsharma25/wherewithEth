afb9c31edb7a14ba0d1d3886f4520509420cfb35
Keshav Sharma <skeshav0825@gmail.com>
2023-01-22 02:03:06 +0530

    Fix: resolved tokenContractPrice error

--- src/utils/assetHelpers.ts
+++ src/utils/assetHelpers.ts
@@ -36,7 +36,7 @@ export const getChainTokenAssets = async (address: string, chain: string) => {
 
     const tokenPrices = await getTokensPrices(tokenCodesInDB);
 
-    if (tokens.length > 0) {
+    if (tokens.length > 0 && tokenPrices) {
       const promises = tokens.map(async (token: TokenType) => {
         if (tokenCodesInDB.includes(token.symbol)) {
           const tokenInDb = tokensInDB.find((t) => t.code === token.symbol);
@@ -75,7 +75,7 @@ export const getChainTokenAssets = async (address: string, chain: string) => {
             platform
           );
 
-          if (tokenPrice.rate) {
+          if (!token?.error && tokenPrice.rate) {
             const quote =
               (tokenPrice.rate * parseInt(token.balance)) /
               Math.pow(10, token.decimals);
